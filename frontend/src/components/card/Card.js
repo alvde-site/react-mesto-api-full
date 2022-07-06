6aeae73b8781ab09d6adb92c732cbe07b13bf236
Aleksandr Demidenko <sashatro@mail.ru>
2022-07-06 11:11:24 +0300

    Исправить отображение лайка карточки

--- frontend/src/components/card/Card.js
+++ frontend/src/components/card/Card.js
@@ -10,7 +10,7 @@ function Card(props) {
     !isOwn && "element__remove-button_hidden"
   }`;
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = props.card.likes.some((i) => i === currentUser._id);
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = `element__like-button ${
     isLiked && "element__like-button_active"
